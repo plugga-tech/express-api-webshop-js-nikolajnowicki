a69c18de32e9d3ca6c96d71a368f64cd7f13a712
nikolajnowicki <114876157+nikolajnowicki@users.noreply.github.com>
2023-03-22 15:13:16 +0100

    Started on category

---
+++ backend/models/products-model.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const ProductSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  price: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  lager: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  category: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  token: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+});
+
+module.exports = mongoose.model("Product", ProductSchema);
--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,16 +1,58 @@
 var express = require("express");
 var router = express.Router();
+const Product = require("../models/products-model");
 
-router.get("/", function (req, res, next) {
-  res.send("Prod router works");
+router.get("/", async function (req, res, next) {
+  const products = await Product.find({});
+  res.status(200).json(products);
+});
+
+router.get("/:id", async function (req, res, next) {
+  const productId = req.params.id;
+  const product = await Product.findById(productId);
+  if (!product) {
+    return res.status(404).json({ message: "Product not found" });
+  }
+  res.status(200).json(product);
 });
 
 router.get("/add", function (req, res, next) {
   res.send("Add products router router");
 });
 
-router.get("/category", function (req, res, next) {
-  res.send("category router");
+router.post("/add", async function (req, res, next) {
+  try {
+    const { name, description, price, lager, category, token } = req.body;
+
+    const product = new Product({
+      name,
+      description,
+      price,
+      lager,
+      category,
+      token,
+    });
+    await product.save();
+    res.status(201).send("Product has been added!");
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server error");
+  }
 });
 
+router.get("/categories/", async function (req, res, next) {
+  return res.status(400).json({ message: "Please specify a category" });
+});
+
+router.get("/category/:category?", async function (req, res, next) {
+  const category = req.params.category;
+  if (!category) {
+    return res.status(400).json({ message: "Please enter a valid category" });
+  }
+  const products = await Product.find({ category: category });
+  if (!products.length) {
+    return res.status(404).json({ message: "No products found" });
+  }
+  res.status(200).json(products);
+});
 module.exports = router;
